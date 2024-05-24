6a93f7262b6091f9fa0950b8b6a7821e2210e481
Gifted Devs <166598671+mouricedevs@users.noreply.github.com>
2024-05-24 15:48:27 +0300

    𝗩𝗜𝗥𝗨𝗦𝗜-𝗠𝗕𝗔𝗬𝗔-𝗠𝗗

---
+++ id.js
@@ -0,0 +1,10 @@
+function giftedid(num = 4) {
+  let result = "";
+  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var characters9 = characters.length;
+  for (var i = 2; i < num; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters9));
+  }
+  return result;
+}
+module.exports = {giftedid};
